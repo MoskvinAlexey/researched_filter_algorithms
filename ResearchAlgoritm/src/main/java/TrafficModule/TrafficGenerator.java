1071daf371e069dc362701f89eadfef5e5c95917
MoskvinAlexey <mskvn91@gmail.com>
2013-03-09 10:34:33 +0400

    some change

--- ResearchAlgoritm/src/main/java/TrafficModule/TrafficGenerator.java
+++ ResearchAlgoritm/src/main/java/TrafficModule/TrafficGenerator.java
@@ -63,7 +63,7 @@ public class TrafficGenerator implements Runnable {
 
 
         try {
-            pcap.loop(-1, jpacketHandler, "");
+            pcap.loop(10, jpacketHandler, "");
         } finally {
 
             pcap.close();
